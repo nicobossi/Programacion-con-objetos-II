3946fbbb2a1d4e1237a6ae93d3d0cb650dfd06dc
Nicolás Bossi <nbossi@hotmail.com>
2025-04-09 08:21:31 -0300

    Modificación de los nombres de los trabajadores para imprimir en pantalla el ppromedio de edad

--- tp3/src/workTestRun/Program.java
+++ tp3/src/workTestRun/Program.java
@@ -13,9 +13,9 @@ public class Program {
 		
 		aTeam.addPerson(new Person("Nicolás", new GregorianCalendar(1995, 5, 16)));
 		aTeam.addPerson(new Person("Matias", new GregorianCalendar(1996, 4, 13)));
-		aTeam.addPerson(new Person("Nicolás", new GregorianCalendar(2000, 11, 12)));
-		aTeam.addPerson(new Person("Nicolás", new GregorianCalendar(2002, 7, 2)));
-		aTeam.addPerson(new Person("Nicolás", new GregorianCalendar(1995, 8, 1)));
+		aTeam.addPerson(new Person("Fernando", new GregorianCalendar(2000, 11, 12)));
+		aTeam.addPerson(new Person("Andrés", new GregorianCalendar(2002, 7, 2)));
+		aTeam.addPerson(new Person("Ahinco", new GregorianCalendar(1995, 8, 1)));
 		
 		System.out.print("El promedio de edad es " + aTeam.ageAverage());
 
